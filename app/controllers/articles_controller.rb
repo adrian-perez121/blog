cfbd7094122b850ae9c56603911819709a87ce6b
Adrian Perez <adrian1804perez@gmail.com>
2024-07-08 13:41:57 -0700

    Add functionality for creating a new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,25 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    # By default this renders the new article view
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      # Recall: Redirect to makes a new request
+      redirect_to @article
+    else # If the user missed something
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
